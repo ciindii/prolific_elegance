dede77bc5364fc1163aca195c352596ac2eec351
ciindii <cindythitruong@gmail.com>
2018-12-06 23:15:54 -0800

    side navigation

--- scripts.js
+++ scripts.js
@@ -3,3 +3,21 @@ $(document).ready(function () {
     $('.icon').toggleClass('active');
   });
 });
+
+function myFunction() {
+  document.getElementById('toggle-nav').classList.toggle('show');
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches('.icon')) {
+
+    var dropdowns = document.getElementsByClassName('drop-content');
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+};
