492bfefe9a1147ddabce7c1bcbce9cd727292510
ciindii <cindythitruong@gmail.com>
2018-12-23 01:00:58 -0800

    added portolio photos

--- scripts.js
+++ scripts.js
@@ -97,3 +97,23 @@ $('#toggleMeetOwner').on('click', function () {
     $('#toggleDiv').toggleClass('show-div').toggleClass('hide-div');
     open = false;
   });
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName('mySlides');
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = 'none';
+  }
+
+  myIndex++;
+  if (myIndex > x.length) {
+    myIndex = 1;
+  }
+
+  x[myIndex - 1].style.display = 'block';
+
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
