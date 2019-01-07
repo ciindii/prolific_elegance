ffd8806427b387209215964b4e201ac0f60fa124
ciindii <cindythitruong@gmail.com>
2019-01-07 15:55:40 -0800

    started particles.js

--- scripts.js
+++ scripts.js
@@ -98,22 +98,22 @@ $('#toggleMeetOwner').on('click', function () {
     open = false;
   });
 
-var myIndex = 0;
-carousel();
-
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName('mySlides');
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = 'none';
-  }
-
-  myIndex++;
-  if (myIndex > x.length) {
-    myIndex = 1;
-  }
-
-  x[myIndex - 1].style.display = 'block';
-
-  setTimeout(carousel, 2000); // Change image every 2 seconds
-}
+// var myIndex = 0;
+// carousel();
+//
+// function carousel() {
+//   var i;
+//   var x = document.getElementsByClassName('mySlides');
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = 'none';
+//   }
+//
+//   myIndex++;
+//   if (myIndex > x.length) {
+//     myIndex = 1;
+//   }
+//
+//   x[myIndex - 1].style.display = 'block';
+//
+//   setTimeout(carousel, 2000); // Change image every 2 seconds
+// }
